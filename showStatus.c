a84998699b4919bb7574858599a1b82d91819291
amxsupport <itmasterdesigne@gmail.com>
2023-06-13 12:18:35 +0000

    101-rot13

---
+++ showStatus.c
@@ -0,0 +1,15 @@
+/*import subprocess*/
+
+/* def git_add_commit_push():
+    # Add all files to the Git repository
+    subprocess.run(["git", "status"])
+
+    git_add_commit_push()*/
+#include <stdio.h>
+#include <string.h>
+
+	int main(){
+		
+		printf("Hellow Vagrant");
+	
+	}
